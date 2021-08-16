b0b80f33e1d99d9709f0032bedb817aa67fab558
Voltair88 <70101120+Voltair88@users.noreply.github.com>
2021-08-16 18:45:17 +0200

    first and only push.
    it was done in a few hours.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Accordion } from "@material-ui/core";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Body } from "./styles";
 
 function App() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/")
+      .then((response) => response.json())
+      .then((json) => {
+        setPeople(json.results);
+      });
+  }, []);
+  const search = (term) => {
+    fetch("https://swapi.dev/api/people/?search=" + term)
+      .then((response) => response.json())
+      .then((json) => {
+        setPeople(json.results);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body>
+      <div className="people">
+        <input
+          type="text"
+          placeholder="Search for a person"
+          onChange={(e) => search(e.target.value)}
+          className="search"
+        />
+        {people.map((person) => (
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              {person.name}
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className="person-details">
+                <span>Height: {person.height}Cm </span> <br />
+                <span>Mass: {person.mass}Kg </span> <br />
+                <span>Hair Color: {person.hair_color} </span> <br />
+                <span>Eye Color: {person.eye_color} </span> <br />
+                <span>Birth Year: {person.birth_year} </span> <br />
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </Body>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+export const Body = styled.body`
+  background-color: #fafafa;
+  background-color: white;
+  font-family: "Lucida Grande", Verdana, Arial, sans-serif;
+  font-size: 12px;
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  height: 100%;
+
+  .content {
+    width: 100%;
+    height: 100%;
+    text-align: left;
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    color: #333;
+    border-radius: 3px;
+    padding: 5px;
+    border: 1px solid #333;
+    box-shadow: 0 0 5px #333;
+    -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -ms-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+  }
+
+  .search {
+    width: 98%;
+    height: 95%;
+    text-align: center;
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    color: #333;
+    border-radius: 3px;
+    padding: 5px;
+    border: 1px solid #333;
+    box-shadow: 0 0 5px #333;
+    -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -ms-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+  }
+
+  .people {
+    cursor: pointer;
+    margin: auto;
+    width: 80%;
+    height: 100%;
+    text-align: center;
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    color: #333;
+    border-radius: 3px;
+    padding: 5px;
+    border: 1px solid #333;
+    box-shadow: 0 0 5px #333;
+    -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -ms-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+  }
+
+  .person-details {
+    padding: 5px;
+    border-radius: 3px;
+    border: 1px solid #333;
+    box-shadow: 0 0 5px #333;
+    background-color: #fff;
+    color: #333;
+    font-size: 12px;
+    font-weight: bold;
+    text-align: left;
+    height: 100%;
+    overflow: hidden;
+    -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -ms-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+  }
+`;
