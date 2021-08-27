251d60e8f45ced185cccee0a7db8f3c376ac44d1
Voltair88 <70101120+Voltair88@users.noreply.github.com>
2021-08-27 16:11:38 +0200

    some rework

--- src/App.js
+++ src/App.js
@@ -1,70 +1,12 @@
-import { useState, useEffect } from "react";
-import { Accordion } from "@material-ui/core";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Body } from "./styles";
+import { List } from "./components/swpeople"
+
 
 function App() {
-  const [people, setPeople] = useState([]);
-
-  useEffect(() => {
-    fetch("https://swapi.dev/api/people/")
-      .then((response) => response.json())
-      .then((json) => {
-        setPeople(json.results);
-      });
-  }, []);
-  const search = (term) => {
-    fetch("https://swapi.dev/api/people/?search=" + term)
-      .then((response) => response.json())
-      .then((json) => {
-        setPeople(json.results);
-      });
-  };
   return (
-    <Body>
-      <div className="people">
-        <input
-          type="text"
-          placeholder="Search for a person"
-          onChange={(e) => search(e.target.value)}
-          className="search"
-        />
-
-        {people.map((person) => (
-          <Accordion key={person.name}>
-            <AccordionSummary
-              style={{ padding: "0px" }}
-              expandIcon={<ExpandMoreIcon />}
-            >
-              {person.name}
-            </AccordionSummary>
-            <p>test 2</p> 
-            <AccordionDetails>
-              <div className="person-details">
-                <p>
-                  <strong>Birth Year: </strong> {person.birth_year}
-                </p>
-                <p>
-                  <strong>Hair:</strong> {person.hair_color}
-                </p>
-                <p>
-                  <strong>Eye Color:</strong> {person.eye_color}
-                </p>
-                <p>
-                  <strong>Height:</strong> {person.height}
-                </p>
-                <p>
-                  <strong>Weight:</strong> {person.mass}
-                </p>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-        ))}
-      </div>
-    </Body>
-  );
+    <div>
+      <List />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/swpeople.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { Accordion } from "@material-ui/core";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { fetchData } from "../utils/api"
+
+export function List() {
+    const [people, setPeople] = useState([])
+
+useEffect(() => {
+    fetchData().then(response => setPeople(response))
+  }, [])
+
+  const search = (term) => {
+    fetch("https://swapi.dev/api/people/?search=" + term)
+      .then((response) => response.json())
+      .then((json) => {
+        setPeople(json.results);
+      });
+  };
+  return (
+    <div>
+      <div className="people">
+         <input
+          type="text"
+          placeholder="Search for a person"
+          onChange={(e) => search(e.target.value)}
+          className="search"
+        />  
+        {people.map((person) => (
+          <Accordion key={person.name}>
+            <AccordionSummary
+              style={{ padding: "0px" }}
+              expandIcon={<ExpandMoreIcon />}
+            >
+              {person.name}
+            </AccordionSummary>
+            <p>test 2</p> 
+            <AccordionDetails>
+              <div className="person-details">
+                <p>
+                  <strong>Birth Year: </strong> {person.birth_year}
+                </p>
+                <p>
+                  <strong>Hair:</strong> {person.hair_color}
+                </p>
+                <p>
+                  <strong>Eye Color:</strong> {person.eye_color}
+                </p>
+                <p>
+                  <strong>Height:</strong> {person.height}
+                </p>
+                <p>
+                  <strong>Weight:</strong> {person.mass}
+                </p>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </div>
+  );
+};
