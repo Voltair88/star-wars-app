64ca5c166fb514c0ba6d99284de3428ee75574d3
Mattias Gulec <Voltair88@gmail.com>
2022-04-29 17:58:57 +0200

    added full list of people

--- src/components/swpeople.js
+++ src/components/swpeople.js
@@ -3,14 +3,14 @@ import { Accordion } from "@material-ui/core";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { fetchData } from "../utils/api"
+import { fetchData } from "../utils/api";
 
 export function List() {
-    const [people, setPeople] = useState([])
+  const [people, setPeople] = useState([]);
 
-useEffect(() => {
-    fetchData().then(response => setPeople(response))
-  }, [])
+  useEffect(() => {
+    fetchData().then((response) => setPeople(response));
+  }, []);
 
   const search = (term) => {
     fetch("https://swapi.dev/api/people/?search=" + term)
@@ -19,46 +19,52 @@ useEffect(() => {
         setPeople(json.results);
       });
   };
+
+  //fetchdata is loading
+
   return (
     <div>
       <div className="people">
-         <input
+        <input
           type="text"
           placeholder="Search for a person"
           onChange={(e) => search(e.target.value)}
           className="search"
-        />  
-        {people.map((person) => (
-          <Accordion key={person.name}>
-            <AccordionSummary
-              style={{ padding: "0px" }}
-              expandIcon={<ExpandMoreIcon />}
-            >
-              {person.name}
-            </AccordionSummary>
-            <p>test 2</p> 
-            <AccordionDetails>
-              <div className="person-details">
-                <p>
-                  <strong>Birth Year: </strong> {person.birth_year}
-                </p>
-                <p>
-                  <strong>Hair:</strong> {person.hair_color}
-                </p>
-                <p>
-                  <strong>Eye Color:</strong> {person.eye_color}
-                </p>
-                <p>
-                  <strong>Height:</strong> {person.height}
-                </p>
-                <p>
-                  <strong>Weight:</strong> {person.mass}
-                </p>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-        ))}
+        />
+        {people.length < 1 ? (
+          <div>Loading...</div>
+        ) : (
+          people.map((person) => (
+            <Accordion key={person.name}>
+              <AccordionSummary
+                style={{ padding: "0px" }}
+                expandIcon={<ExpandMoreIcon />}
+              >
+                {person.name}
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className="person-details">
+                  <p>
+                    <strong>Birth Year: </strong> {person.birth_year}
+                  </p>
+                  <p>
+                    <strong>Hair:</strong> {person.hair_color}
+                  </p>
+                  <p>
+                    <strong>Eye Color:</strong> {person.eye_color}
+                  </p>
+                  <p>
+                    <strong>Height:</strong> {person.height}
+                  </p>
+                  <p>
+                    <strong>Weight:</strong> {person.mass}
+                  </p>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          ))
+        )}
       </div>
     </div>
   );
-};
+}
